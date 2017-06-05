0bc06fafd6ef2b19a084f3ee117c926d41a190c0
Owl <shevonnechia1@gmail.com>
2017-06-05 16:23:55 +0800

    Pseudocode added.

--- 主.cpp
+++ 主.cpp
@@ -1,4 +1,29 @@
+//this is all pseudocode
+bool ready = false;
+bool inserted = false;
+bool solving = false;
+void long_press()
+{
+    ready = false;
+    inserted = false;
+    solving = false;
+}
+
+void short_press()
+{
+    if (!ready)
+    {
+        ready = true;
+        return nil;
+    }
+    else if (!inserted)
+    {
+        inserted = true;
+        return nil;
+    }
+}
+
 int main()
 {
-    //Lemon > Apple > Orange
+    
 }
