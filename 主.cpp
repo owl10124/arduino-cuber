515403670cff9acbf033ca740bcaf1ed2a122ecf
Owl <shevonnechia1@gmail.com>
2017-06-15 22:43:17 +0900

    I'm just attempting to prove that I did *something* (well it's negligible but still.)

--- 主.cpp
+++ 主.cpp
@@ -1,7 +1,15 @@
 //this is all pseudocode
+#include <ctime>
+#include <iostream>
+#include <fstream>
+#include <raspicam/raspicam_cv.h>
+using namespace std;
+
 bool ready = false;
 bool inserted = false;
 bool solving = false;
+string path = "/dev/ttyUSB0"; //idek work it out yourself
+
 void long_press()
 {
     ready = false;
@@ -25,5 +33,20 @@ void short_press()
 
 int main()
 {
+    fstream ardFile;
+    raspicam::RaspiCam Camera;
+
+    //camera stuff
+    cout<<"Opening camera\n";
+    if (!Camera.open())
+    {
+        cerr<<"Error opening camera\n";
+    }
     
+    //establish serial connection
+    ardFile = fstream::open(path, fstream::in|fstream::out)
+    while (true)
+    {
+        
+    }
 }
