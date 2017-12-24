404643d5a0871a64699658a1dab49b9a9bd89ae3
Owl <shevonnechia1@gmail.com>
2017-12-24 13:59:35 +0800

    it compiles.

--- ImageRecognition/main.sh
+++ ImageRecognition/main.sh
@@ -1,1 +1,2 @@
-g++ -Wall ./main.cpp -I/usr/local/include -lraspicam -L/opt/vc/lib -lmmal -lmmal_core -lmmal_util -lwiringPi -std=c++11
+g++ -Wall ./main.cpp -o imageprocessing -I/usr/local/include -lraspicam -lraspicam_cv -lopencv_core -lopencv_highgui -lopencv_imgproc -L/opt/vc/lib -lmmal -lmmal_core -lmmal_util -lwiringPi -std=c++11
+
